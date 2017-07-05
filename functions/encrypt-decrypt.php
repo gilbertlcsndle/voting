03d9ce535a8f71e6454f678306c6662ba26e372d
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2017-07-05 21:29:22 +0800

    Fix and update encrypt function

--- functions/encrypt-decrypt.php
+++ functions/encrypt-decrypt.php
@@ -1,7 +1,7 @@
 <?php 
 function encrypt_pass($input_pass) {
     $chars = substr(sha1(mt_rand()), 0, 22);
-    $salt  = sprintf('$2y$10%1$s', $chars);
+    $salt  = sprintf('$2x$10$%s', $chars);
 
     return crypt($input_pass, $salt);
 }
